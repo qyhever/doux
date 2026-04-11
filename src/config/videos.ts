c3381c1bc11d7b6dbe6b2289a33a3402b85fde56
qyhever <qyhever@126.com>
2026-04-11 22:05:18 +0800

    package.json

--- src/config/videos.ts
+++ src/config/videos.ts
@@ -1,7 +1,12 @@
 export const VIDEO_URLS = [
-  'https://www.w3schools.com/html/mov_bbb.mp4',
-  'https://www.w3schools.com/html/movie.mp4',
-  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/24u7qivyunz.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/2hzcohudabj.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/a9d922da.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/a9d922dc.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/a9d922e1.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/a9d92409.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/d4eqp6p7vmm.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/r5u00mc02j.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/v28dddg7yvr.mp4',
+  'https://pub-3ee37ba039ed4f3d848a6de6da8eb205.r2.dev/vjsab8nx29p.mp4',
 ];
